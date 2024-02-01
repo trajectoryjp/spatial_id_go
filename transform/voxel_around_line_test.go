07903cd6ef7f6cae2d1c1dc5ddc05c8befa2e2f5
Tom <56852146+buscandoaverroes@users.noreply.github.com>
2024-02-01 15:18:29 +0900

    improve TestFitClearanceAroundExtendedSpatialID
    add testFitClearanceAroundExtendedSpatialID and add various Test functions

--- transform/voxel_around_line_test.go
+++ transform/voxel_around_line_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-gl/mathgl/mgl64"
 	closest "github.com/trajectoryjp/closest_go"
 	geodesy "github.com/trajectoryjp/geodesy_go/coordinates"
+	"github.com/trajectoryjp/spatial_id_go/common/errors"
 	"github.com/trajectoryjp/spatial_id_go/common/object"
 	"github.com/trajectoryjp/spatial_id_go/shape"
 )
@@ -46,65 +47,119 @@ func TestGetSpatialIdsWithinRadiusOfLine(t *testing.T) {
 
 }
 
-func TestGetSpatialIdsWithinRadiusOfLine_skipsMeasurement(t *testing.T) {
+func TestFitClearanceAroundExtendedSpatialID01(t *testing.T) {
 
-	// sumida river, 200m
-	startPoint, error := object.NewPoint(139.788452, 35.670935, 100)
-	if error != nil {
-		t.Error(error)
-	}
-	endPoint, error := object.NewPoint(139.788074, 35.761311, 100)
-	if error != nil {
-		t.Error(error)
-	}
-
-	// measure distance
-	startCartesian := geodesy.GeocentricFromGeodetic(geodesy.Geodetic{startPoint.Lon(), startPoint.Lat(), startPoint.Alt()})
-	endCartesian := geodesy.GeocentricFromGeodetic(geodesy.Geodetic{endPoint.Lon(), endPoint.Lat(), endPoint.Alt()})
-
-	measure := closest.Measure{}
-	measure.ConvexHulls[0] = []*mgl64.Vec3{(*mgl64.Vec3)(&startCartesian)}
-	measure.ConvexHulls[1] = []*mgl64.Vec3{(*mgl64.Vec3)(&endCartesian)}
-
-	measure.MeasureDistance()
-
-	t.Logf("Distance: %vm", measure.Distance)
-
-	start := time.Now()
-	spatialIds, error := GetSpatialIdsWithinRadiusOfLine(startPoint, endPoint, 5, 23, 23, true)
-	elapsed := time.Since(start)
-	if error != nil {
-		t.Error(error)
-	}
-
-	t.Logf("\n%v Spatial IDs found in %v \n", len(spatialIds), elapsed)
-
-}
-
-func TestFitClearanceAroundExtendedSpatialID(t *testing.T) {
+	var clearance float64 = 0
+	var expectedHLayer int64 = 1
+	var expectedVLayer int64 = 1
 
 	point, error := object.NewPoint(139.788081, 35.672680, 100)
 	if error != nil {
 		t.Error(error)
 	}
+	hLayer, vLayer, error := testFitClearanceAroundSpatialID(t, point, clearance, 25, 25)
+	if error != nil {
+		t.Error(error)
+	}
+
+	if hLayer != expectedHLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedHLayer, hLayer)
+	}
+	if vLayer != expectedVLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedVLayer, vLayer)
+	}
+
+}
+
+func TestFitClearanceAroundExtendedSpatialID02(t *testing.T) {
+
+	var clearance float64 = 10
+	var expectedHLayer int64 = 11
+	var expectedVLayer int64 = 11
+
+	point, error := object.NewPoint(139.788081, 35.672680, 100)
+	if error != nil {
+		t.Error(error)
+	}
+	hLayer, vLayer, error := testFitClearanceAroundSpatialID(t, point, clearance, 25, 25)
+	if error != nil {
+		t.Error(error)
+	}
+
+	if hLayer != expectedHLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedHLayer, hLayer)
+	}
+	if vLayer != expectedVLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedVLayer, vLayer)
+	}
+
+}
+
+func TestFitClearanceAroundExtendedSpatialID03(t *testing.T) {
+
+	var clearance float64 = 10
+	var expectedHLayer int64 = 3
+	var expectedVLayer int64 = 3
+
+	point, error := object.NewPoint(139.788081, 35.672680, 100)
+	if error != nil {
+		t.Error(error)
+	}
+	hLayer, vLayer, error := testFitClearanceAroundSpatialID(t, point, clearance, 23, 23)
+	if error != nil {
+		t.Error(error)
+	}
+
+	if hLayer != expectedHLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedHLayer, hLayer)
+	}
+	if vLayer != expectedVLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedVLayer, vLayer)
+	}
+
+}
+
+func TestFitClearanceAroundExtendedSpatialID04(t *testing.T) {
+
+	var clearance float64 = 50
+	var expectedHLayer int64 = 4
+	var expectedVLayer int64 = 4
+
+	point, error := object.NewPoint(139.788081, 35.672680, 100)
+	if error != nil {
+		t.Error(error)
+	}
+	hLayer, vLayer, error := testFitClearanceAroundSpatialID(t, point, clearance, 21, 21)
+	if error != nil {
+		t.Error(error)
+	}
+
+	if hLayer != expectedHLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedHLayer, hLayer)
+	}
+	if vLayer != expectedVLayer {
+		t.Errorf("空間ID - 期待値：%v, 取得値：%v", expectedVLayer, vLayer)
+	}
+
+}
+
+func testFitClearanceAroundSpatialID(t *testing.T, point *object.Point, clearance float64, hZoom int64, vZoom int64) (hLayer int64, vlayer int64, error error) {
+
 	points := []*object.Point{point}
-	// 25, 25 is approx 1m box
-	spatialId, error := shape.GetExtendedSpatialIdsOnPoints(points, 25, 25)
+	// 25, 25 is just over 1m box
+	spatialId, error := shape.GetExtendedSpatialIdsOnPoints(points, hZoom, vZoom)
 	if error != nil {
-		t.Error(error)
+		return 0, 0, error
 	}
-
 	if len(spatialId) != 1 {
-		t.Fatalf("N. Id's != 1")
+		return 0, 0, errors.NewSpatialIdError(errors.OtherErrorCode, "returned more than 1 spatialID. Please check point again")
 	}
 
-	start := time.Now()
-	hLayer, vLayer, error := FitClearanceAroundExtendedSpatialID(spatialId[0], 1000)
-	end := time.Since(start)
+	hLayer, vLayer, error := FitClearanceAroundExtendedSpatialID(spatialId[0], clearance)
 	if error != nil {
-		t.Error(error)
+		return 0, 0, error
 	}
-	t.Logf("\nCalculation time: %v\n", end)
-	t.Logf("\nHorizontal Layers: %v\tVertical Layers: %v", hLayer, vLayer)
+
+	return hLayer, vLayer, nil
 
 }
